572e1522d0657cecf42f448710a5e597179f41d3
TanmayGupta17 <TanmayGupta1706@gmail.com>
2025-06-29 19:39:45 +0530

    fixed authentication error

--- src/app/Authentication/page.jsx
+++ src/app/Authentication/page.jsx
@@ -73,7 +73,7 @@ export default function AuthPage() {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(signupData),
+          body: JSON.stringify(loginData),
         }
       );
 
